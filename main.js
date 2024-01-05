fe525ac8f4206c6f06e7c5897126e1f60ae6cd6c
Suman biswas <120238416+Gdllll@users.noreply.github.com>
2024-01-05 20:04:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function validate(){
+  if(document.getElementById("text1").value == "suman" && document.getElementById("text2").value == "2332")
+  {
+  alert("validation succededed");
+  location.href = "run.html";
+  
+    
+    
+  }
+
+    if (document.getElementById("text1").value == "raj" && document.getElementById("text2").value == "2222")
+    {
+      alert("validation succededed");
+      location.href = "run.html";
+  
+  
+  
+    }
+
+      if (document.getElementById("text1").value == "or" && document.getElementById("text2").value == "5555")
+      {
+        alert("validation succededed");
+        location.href = "run.html";
+    
+    
+    
+      }
+      
+      if (document.getElementById("text1").value == "shubhomoy" && document.getElementById("text2").value == "555555")
+      {
+        alert("validation succededed");
+        location.href = "run.html";
+      
+      
+      
+      }
+       if (document.getElementById("text1").value == "sunando" && document.getElementById("text2").value == "sunando1234")
+       {
+         alert("validation succededed");
+         location.href = "run.html";
+
+       }
+     
+}
